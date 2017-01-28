3327a4d189100ce44718c162cf1320b0e47c8517
gurv <vladimir.gurinovich@gmail.com>
2017-01-28 13:45:00 +0300

    FIX logging

--- web/src/app/currency/currency-dasshboard.component.ts
+++ web/src/app/currency/currency-dasshboard.component.ts
@@ -7,7 +7,7 @@ import {Component, OnInit} from '@angular/core';
 export class CurrencyDashboardComponent implements OnInit {
 
   ngOnInit() {
-    console.log('Инициализация компонента `CurrencyRateDashboard`');
+    console.log('Инициализация компонента `CurrencyDashboard`');
   }
 
 }
