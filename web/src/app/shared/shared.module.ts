03f605dbf00c1844af0b27321a68bc66d40407d3
gurv <vladimir.gurinovich@gmail.com>
2017-12-18 22:42:50 +0300

    feat (web): MatTableModule

--- web/src/app/shared/shared.module.ts
+++ web/src/app/shared/shared.module.ts
@@ -16,6 +16,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTableModule } from "@angular/material";
 
 @NgModule({
   imports: [
@@ -35,7 +36,8 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatListModule,
     MatMenuModule,
     MatIconModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatTableModule
   ],
   exports: [
     CommonModule,
@@ -55,6 +57,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatToolbarModule,
     MatIconModule,
     MatTooltipModule,
+    MatTableModule
   ]
 })
 export class SharedModule {}
