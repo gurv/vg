f5d7d02a99cd158baa8d2aff353c483293de2598
gurv <vladimir.gurinovich@gmail.com>
2017-12-20 20:52:30 +0300

    feat (web): google map

--- web/src/app/static/dashboard/dashboard.module.ts
+++ web/src/app/static/dashboard/dashboard.module.ts
@@ -4,9 +4,10 @@ import {CommonModule} from '@angular/common';
 import {DashboardComponent} from './dashboard.component';
 import {SharedModule} from "@app/shared";
 import {ToolchainModule} from "@app/static/dashboard/toolchain/toolchain.module";
+import {MapModule} from "@app/static/dashboard/map/map.module";
 
 @NgModule({
-  imports: [CommonModule, SharedModule, ToolchainModule],
+  imports: [CommonModule, SharedModule, ToolchainModule, MapModule],
   declarations: [DashboardComponent]
 })
 export class DashboardModule {
