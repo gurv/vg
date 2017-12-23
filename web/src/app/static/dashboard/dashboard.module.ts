473998847cc246ca455e1c0216d1dc7f33faaa6d
gurv <vladimir.gurinovich@gmail.com>
2017-12-23 17:02:58 +0300

    feat (web): weather

--- web/src/app/static/dashboard/dashboard.module.ts
+++ web/src/app/static/dashboard/dashboard.module.ts
@@ -6,9 +6,12 @@ import {SharedModule} from "@app/shared";
 import {ToolchainModule} from "@app/static/dashboard/toolchain/toolchain.module";
 import {MapModule} from "@app/static/dashboard/map/map.module";
 import {CurrencyModule} from "@app/static/dashboard/currency/currency.module";
+import {WeatherModule} from "@app/static/dashboard/weather/weather.module";
 
 @NgModule({
-  imports: [CommonModule, SharedModule, ToolchainModule, MapModule, CurrencyModule],
+  imports: [CommonModule, SharedModule, ToolchainModule, MapModule, CurrencyModule,
+    WeatherModule
+  ],
   declarations: [DashboardComponent]
 })
 export class DashboardModule {
