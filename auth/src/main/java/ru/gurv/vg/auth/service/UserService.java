8131f7da3b83f9445732b3e08327711fa0ebb19d
gurv <vladimir.gurinovich@gmail.com>
2017-12-29 20:53:43 +0300

    drop auth

--- auth/src/main/java/ru/gurv/vg/auth/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package ru.gurv.vg.auth.service;
-
-import ru.gurv.vg.auth.domain.User;
-
-public interface UserService {
-
-	void create(User user);
-
-}
--- auth/src/main/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE User
-  (
-  userName VARCHAR(100) NOT NULL,
-  password VARCHAR(100) DEFAULT NULL,
-  PRIMARY KEY (userName)
-  );
