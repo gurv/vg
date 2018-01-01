0535b5ced03de8046d42e2f88d78bf9dea3da4cc
gurv <vladimir.gurinovich@gmail.com>
2018-01-01 22:48:13 +0300

    feat (auth): oidc

---
+++ auth/src/main/java/ru/gurv/vg/auth/controller/UserController.java
@@ -0,0 +1,15 @@
+package ru.gurv.vg.auth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/")
+    String home(Principal user) {
+        return "Hello " + user.getName();
+    }
+}
