6c0930996bff826ac39642b4f4debf2c2d5374af
gurv <vladimir.gurinovich@gmail.com>
2018-03-03 17:19:16 +0300

    build (backend): spring

--- account/src/main/java/ru/gurv/vg/account/AccountApplication.java
+++ account/src/main/java/ru/gurv/vg/account/AccountApplication.java
@@ -11,7 +11,7 @@ import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceS
 */
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
--- account/src/main/java/ru/gurv/vg/account/client/AuthServiceClient.java
+++ account/src/main/java/ru/gurv/vg/account/client/AuthServiceClient.java
@@ -1,7 +1,7 @@
 package ru.gurv.vg.account.client;
 
 import ru.gurv.vg.account.domain.User;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
