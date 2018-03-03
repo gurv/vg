2c7390f1d78f86a511aed31d8aacd62fb8960ad8
gurv <vladimir.gurinovich@gmail.com>
2018-03-03 17:27:22 +0300

    feat (sample): operation

--- sample/operation/src/main/java/ru/gurv/vg/sample/operation/OperationSampleApplication.java
+++ sample/operation/src/main/java/ru/gurv/vg/sample/operation/OperationSampleApplication.java
@@ -2,8 +2,10 @@ package ru.gurv.vg.sample.operation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 
 @SpringBootApplication
+@EnableBinding
 public class OperationSampleApplication {
 
     public static void main(String[] args) {
