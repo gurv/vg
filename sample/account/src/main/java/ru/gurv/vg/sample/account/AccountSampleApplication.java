606e7c85dd982a84311f64eab06d1653755af7f6
gurv <vladimir.gurinovich@gmail.com>
2018-03-09 08:50:36 +0300

    feat (...): spring, docker, build, samples

---
+++ sample/account/src/main/java/ru/gurv/vg/sample/account/AccountSampleApplication.java
@@ -0,0 +1,13 @@
+package ru.gurv.vg.sample.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountSampleApplication.class, args);
+    }
+
+}
