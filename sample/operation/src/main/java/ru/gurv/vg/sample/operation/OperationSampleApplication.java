efa252b883c0b0bab12526a2a065b2cf7b2080dd
gurv <vladimir.gurinovich@gmail.com>
2018-03-10 03:29:10 +0300

    clean

--- sample/operation/src/main/java/ru/gurv/vg/sample/operation/OperationSampleApplication.java
+++ sample/operation/src/main/java/ru/gurv/vg/sample/operation/OperationSampleApplication.java
@@ -1,21 +1,11 @@
 package ru.gurv.vg.sample.operation;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
-import org.springframework.context.annotation.Bean;
-import org.springframework.integration.annotation.InboundChannelAdapter;
-import org.springframework.integration.annotation.Poller;
-import org.springframework.integration.core.MessageSource;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @SpringBootApplication
 @EnableBinding(Source.class)
@@ -23,22 +13,7 @@ import java.util.Date;
 @EnableSwagger2
 public class OperationSampleApplication {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(OperationSampleApplication.class);
-
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
-
     public static void main(String[] args) {
         SpringApplication.run(OperationSampleApplication.class, args);
     }
-
-/*
-    @Bean
-    @InboundChannelAdapter(value = Source.OUTPUT, poller = @Poller(fixedDelay = "10000", maxMessagesPerPoll = "1"))
-    public MessageSource<Date> timeMessageSource() {
-        LOGGER.info("The time is now {}", dateFormat.format(new Date()));
-        return () -> MessageBuilder
-                .withPayload(new Date())
-                .build();
-    }
-*/
 }
