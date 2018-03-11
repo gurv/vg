ce7de4e45d089ceb08404e5302c60ad763920b39
gurv <vladimir.gurinovich@gmail.com>
2018-03-11 11:48:22 +0300

    sample & discovery

--- sample/account/src/main/java/ru/gurv/vg/sample/account/AccountSampleApplication.java
+++ sample/account/src/main/java/ru/gurv/vg/sample/account/AccountSampleApplication.java
@@ -2,9 +2,11 @@ package ru.gurv.vg.sample.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableSwagger2
 public class AccountSampleApplication {
 
--- sample/operation/src/main/java/ru/gurv/vg/sample/operation/OperationSampleApplication.java
+++ sample/operation/src/main/java/ru/gurv/vg/sample/operation/OperationSampleApplication.java
@@ -2,12 +2,14 @@ package ru.gurv.vg.sample.operation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableBinding(Source.class)
 @EnableScheduling
 @EnableSwagger2
